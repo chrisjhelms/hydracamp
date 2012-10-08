feffc3e977fce76423279a6723a3e6bd3f25656f
Chris Helms <chris@merovingian>
2012-10-08 11:08:09 -0400

    Minor syntax update to Zombie model

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -1,6 +1,5 @@
 class Zombie < ActiveRecord::Base
   attr_accessible :graveyard, :name
   
-  validates :name, :presence => true
-  validates_uniqueness_of :name
+  validates :name, :presence => true, :uniqueness => true
 end
